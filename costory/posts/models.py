b2e3e5df7215956ccb3e004deacbfd38400fb883
OSHMOS <ojames9707@gmail.com>
2022-03-02 23:59:58 +0900

    두 번째 레슨 시작

---
+++ costory/posts/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+class Post(models.Model):
+    # 글의 제목, 내용, 작성일, 마지막 수정일
+    title = models.CharField(max_length=50)
+    content = models.TextField() # 길이에 대한 제한이 없는 문자열 필드
+    dt_created = models.DateTimeField(verbose_name="Date Created", auto_now_add=True)
+    # 알아보기 쉽게 별명을 붙여준 것
+    dt_modified = models.DateTimeField(verbose_name="Date Modified", auto_now=True)
+    # auto_now_add => 데이터가 생성될 때, auto_now => 데이터가 수정될 때의 시간 저장
+
+    def __str__(self):
+        return self.title
