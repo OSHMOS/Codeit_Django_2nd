45fc10bca1e23f3dc5777781c48d94436ba1314a
OSHMOS <ojames9707@gmail.com>
2022-03-05 22:51:23 +0900

    form validator, ValidationError

--- costory/posts/models.py
+++ costory/posts/models.py
@@ -1,11 +1,16 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
+from .validators import validate_symbols
 
 # Create your models here.
 
 class Post(models.Model):
     # 글의 제목, 내용, 작성일, 마지막 수정일
-    title = models.CharField(max_length=50)
-    content = models.TextField() # 길이에 대한 제한이 없는 문자열 필드
+    title = models.CharField(max_length=50, unique=True, 
+                             error_messages={'unique' : '이미 있는 제목이네요!'})
+    content = models.TextField(validators=
+                               [MinLengthValidator(10, '너무 짧군요! 10자 이상 적어주세요.'),
+                                validate_symbols]) # 길이에 대한 제한이 없는 문자열 필드
     dt_created = models.DateTimeField(verbose_name="Date Created", auto_now_add=True)
     # 알아보기 쉽게 별명을 붙여준 것
     dt_modified = models.DateTimeField(verbose_name="Date Modified", auto_now=True)
